8dd30595c136c68a2ee655e545036fe7670b267a
tijanihind <tijani.hind200@gmail.com>
2023-07-17 20:34:04 -0400

    modified html and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,51 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   var calendarEl = document.getElementById('calendar');
 
-//   var calendar = new FullCalendar.Calendar(calendarEl, {
-//     initialView: 'dayGridMonth',
-//     headerToolbar: {
-//       center: 'addEventButton'
-//     },
-//     customButtons: {
-//       addEventButton: {
-//         text: 'add event...',
-//         click: function() {
-//           var dateStr = prompt('Enter a date in YYYY-MM-DD format');
-//           var date = new Date(dateStr + 'T00:00:00'); // will be in local time
 
-//           if (!isNaN(date.valueOf())) { // valid?
-//             calendar.addEvent({
-//               title: 'dynamic event',
-//               start: date,
-//               allDay: true
-//             });
-//             alert('Great. Now, update your database...');
-//           } else {
-//             alert('Invalid date.');
-//           }
-//         }
-//       }
-//     }
-//   });
+let apiUrl ="https://api.spoonacular.com/recipes/complexSearch"
+let apiKey ="0821799963dc4a95ab208456cf22646c"
+let diet = document.getElementById("diet")
+let numOfRecipes =30
+let firstName = document.getElementById("first-name")
+let lasttName = document.getElementById("last-name")
+let submitBtn = document.getElementById("submitBtn")
+var submitSec = document.querySelector("#submitSection")
 
-//   calendar.render();
-// });
+let selectedDiet
+diet.addEventListener("change", function() {
+  selectedDiet = this.value
+})
+submitBtn.addEventListener("click", function(event) {
+  console.log(event.target);
+  event.preventDefault();
+  submitSec.setAttribute()
 
-document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
 
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    timeZone: 'EST',
-    initialView: 'timeGridWeek',
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'timeGridWeek,timeGridDay'
-    },
+  fetch(apiUrl + "?apiKey=" + apiKey + "&diet=" + selectedDiet + "&number="+ numOfRecipes)
+  .then(response => response.json())
+  .then(data => {
+    const events = data.results.map((recipe, index) => {
+      return {
+        title: recipe.title,
+        // start date is set to the current month and the day is index+1
+        // this will distribute the recipes throughout the month
+        start: new Date(new Date().getFullYear(), new Date().getMonth(), index + 1).toISOString().split('T')[0]
+      };
+    });
 
-  });
+    var calendarEl = document.getElementById('calendar');
 
-  calendar.render();
-});
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        // this is to remove the arrows that change the month
+        headerToolbar: {
+          start: '',
+          center: 'title',
+          end: ''
+      },
+        events: events
+    });
 
-document.addEventListener(click, )
+    calendar.render();
+  })
+  .catch(error => console.error('Error:', error));
+
+})
