aacd48b7167d8c058a3a313fd1d2bade74a6cbc6
neuemarkets <gabriel@neuemarket.com>
2023-07-19 23:28:17 -0400

    adding updated js file, which included the split screen

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -1,3 +1,113 @@
+let diet = document.getElementById("diet");
+let numOfRecipes = 30;
+let firstName = document.getElementById("first-name");
+let lasttName = document.getElementById("last-name");
+let submitBtn = document.getElementById("submitBtn");
+let submitSec = document.querySelector("#submitSection");
+let checkBox = document.getElementById("weather");
+var cityInput = document.getElementById("city");
+var postSubmitBtn = document.getElementById("postsubmit");
+
+document.addEventListener("DOMContentLoaded", function() {
+    let selectedDiet;
+    diet.addEventListener("change", function() {
+        selectedDiet = this.value;
+    });
+    submitBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        submitSec.setAttribute("class", "hide");
+        postSubmitBtn.setAttribute("class", "min-h-screen flex visible");
+        let cityName = cityInput.value || "Atlanta";
+
+        fetch(`http://api.positionstack.com/v1/forward?access_key=e0982359ac7c9fae34b81113f22add45&query=${cityName}`)
+            .then(response => response.json())
+            .then(geoData => {
+                const latitude = geoData.data[0].latitude;
+                const longitude = geoData.data[0].longitude;
+
+
+                // Now fetch the Storm Glass API using obtained longitude and latitute
+                fetch(`https://api.stormglass.io/v2/weather/point?lat=${latitude}&lng=${longitude}&params=airTemperature`, {
+                        headers: {
+                            'Authorization': 'ea939a76-25ce-11ee-a654-0242ac130002-ea939b2a-25ce-11ee-a654-0242ac130002'
+                        }
+                    })
+                    .then(weatherResponse => weatherResponse.json())
+                    .then(weatherData => {
+                        fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=0821799963dc4a95ab208456cf22646c&diet=${selectedDiet}&number=${numOfRecipes}`)
+                            .then((response) => response.json())
+                            .then((data) => {
+                                // Create events for recipes and weather
+                                const events = [];
+
+                                const currentDate = new Date();
+                                const currentDayOfMonth = currentDate.getDate();
+
+                                data.results.forEach((recipe, index) => {
+                                    // Add recipe event to the calendar
+                                    events.push({
+                                        title: recipe.title,
+                                        start: new Date(
+                                                new Date().getFullYear(),
+                                                new Date().getMonth(),
+                                                index + 1
+                                            )
+                                            .toISOString()
+                                            .split("T")[0],
+                                    });
+
+                                    // Add weather event only for the first 7 days
+                                    if (checkBox.checked) {
+                                        if (index < 7) {
+                                            const currentHour = new Date().getHours();
+                                            const dayWeather = weatherData.hours[currentHour]; // Assuming weatherData.hours[currentHour] holds the forecast for the current hour
+                                            console.log(dayWeather);
+                                            if (dayWeather && dayWeather.airTemperature && dayWeather.airTemperature.sg) {
+                                                let temperatureInCelsius = dayWeather.airTemperature.sg;
+                                                let temperatureInFahrenheit = temperatureInCelsius * 9 / 5 + 32;
+                                                events.push({
+                                                    title: 'Temperature in ' + cityName + ' : ' + temperatureInFahrenheit.toFixed(1) + ' °F',
+                                                    start: new Date(
+                                                            currentDate.getFullYear(),
+                                                            currentDate.getMonth(),
+                                                            currentDayOfMonth + index
+                                                        )
+                                                        .toISOString()
+                                                        .split("T")[0],
+                                                });
+                                            } else {
+                                                console.error('Could not find airTemperature for index', index);
+                                            }
+                                        }
+                                    }
+                                });
+
+                                var calendarEl = document.getElementById("calendar");
+
+                                var calendar = new FullCalendar.Calendar(calendarEl, {
+                                    initialView: "dayGridMonth",
+                                    headerToolbar: {
+                                        start: "",
+                                        center: "title",
+                                        end: "",
+                                    },
+                                    events: events,
+                                });
+
+                                calendar.render();
+                            })
+                            .catch((error) => console.error("Error:", error));
+                    })
+                    .catch(error => {
+                        console.error('There has been a problem with your fetch operation:', error);
+                    });
+            })
+            .catch(error => {
+                console.error('There has been a problem with your geocoding fetch operation:', error);
+            });
+    });
+});
+
 var workout = document.getElementById("workout")
 var workoutTime = document.getElementById("time")
 var workoutMuscle = document.getElementById("muscle")
